73787bec033a60359ebcdbb3a51142f280dad9a7
Andy Wei <48671893+Andy0909@users.noreply.github.com>
2024-04-01 15:20:17 +0800

    Update ErrorRetryService.cs

--- Services/ErrorRetryService.cs
+++ Services/ErrorRetryService.cs
@@ -9,105 +9,109 @@ using System.Text.Json;
 
 namespace ScreenShotLambda.Services
 {
+    /// <summary>
+    /// 異常重試服務
+    /// </summary>
     class ErrorRetryService : IErrorRetry
     {
-        // SQS Client
+        /// <summary>
+        /// AWS SQS 物件
+        /// </summary>
         private readonly IAmazonSQS sqsClient;
 
-        // 異常通知 service
+        /// <summary>
+        /// 異常通知物件
+        /// </summary>
         private readonly IErrorNotify errorNotifyService;
 
-        // 設定 JSON 序列化
-        private readonly JsonSerializerOptions options;
+        /// <summary>
+        /// 刪除 queue 物件
+        /// </summary>
+        private readonly IQueueDelete queueDeleteService;
+        
+        /// <summary>
+        /// Lambda Log 物件
+        /// </summary>
+        private readonly ILambdaLogger logger;
 
-        // 建構子
-        public ErrorRetryService(IAmazonSQS sqsClient, IErrorNotify errorNotifyService)
+        /// <summary>
+        /// 建構子
+        /// <param name="sqsClient">sqs client</param>
+        /// <param name="errorNotifyService">異常通知服務</param>
+        /// <param name="queueDeleteService">刪除佇列服務</param>
+        /// <param name="logger">log 紀錄</param>
+        /// </summary>
+        public ErrorRetryService(IAmazonSQS sqsClient, IErrorNotify errorNotifyService, IQueueDelete queueDeleteService, ILambdaLogger logger)
         {
             this.sqsClient = sqsClient;
-
             this.errorNotifyService = errorNotifyService;
-
-            this.options = new JsonSerializerOptions();
+            this.queueDeleteService = queueDeleteService;
+            this.logger = logger;
         }
-
+         
         /// <summary>
         /// 有錯誤的 queue 進行重試
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="context"></param>
-        public async Task retryErrorQueue(SQSEvent.SQSMessage message, ILambdaContext context)
+        public async Task RetryErrorQueue(SQSEvent.SQSMessage message)
         {
             try
             {
-                var queueMessage = JsonConvert.DeserializeObject<QueueMessage>(message.Body);
+                var queueMessage = JsonSerializer.Deserialize<QueueMessage>(message.Body);
+                var screenShotUrl = queueMessage?.screenShotUrl;
+                var retryCount = queueMessage?.retryCount;
 
-                if (queueMessage.retryCount >= 3)
+                if (retryCount >= 3)
                 {
-                    await this.errorNotifyService.sendErrorMessage($"快照已失敗3次，不再進行重試。", context);
+                    await this.errorNotifyService.SendErrorMessage($"快照已失敗3次，不再進行重試。");
                 }
                 else
                 {
-                    try
+                    // 要傳送的 JSON 資料
+                    var queueData = new
                     {
-                        var retryCount = queueMessage.retryCount + 1;
-
-                        // 要傳送的 JSON 資料
-                        var queueData = new
-                        {
-                            screenShotUrl = queueMessage.screenShotUrl,
-                            retryCount = retryCount,
-                        };
-                        var jsonQueueData = System.Text.Json.JsonSerializer.Serialize(queueData, this.options);
-
-                        // SQS 訊息格式
-                        var request = new SendMessageRequest
-                        {
-                            QueueUrl = "your queue url",  // ex. https://sqs.ap-northeast-1.amazonaws.com/123456789/ScreenShot_Queue
-                            MessageBody = jsonQueueData,
-                        };
-
-                        // 傳送訊息至 SQS 進行重試
-                        var sendMessageResponse = await this.sqsClient.SendMessageAsync(request);
-
-                        context.Logger.LogInformation($"快照重試第{retryCount}次，傳送訊息至 SQS 成功");
-
-                        // 刪除原本錯誤的訊息
-                        await this.deleteErrorMessage(message, context);
-
-                        context.Logger.LogInformation($"快照重試第{retryCount}次，刪除舊訊息成功");
-                    }
-                    catch (AmazonSQSException e)
+                        screenShotUrl = screenShotUrl,
+                        retryCount = retryCount + 1,
+                    };
+                
+                    string jsonQueueData = JsonSerializer.Serialize(queueData);
+                
+                    // SQS 訊息格式
+                    SendMessageRequest request = new SendMessageRequest
                     {
-                        await this.errorNotifyService.sendErrorMessage($"傳送訊息至 SQS 錯誤：{e.Message}。", context);
+                        QueueUrl = Environment.GetEnvironmentVariable("SQS_QUEUE_URL") ?? "",
+                        MessageBody = jsonQueueData,
+                    };
+                
+                    // 傳送訊息至 SQS 進行重試
+                    SendMessageResponse sendMessageResponse = await this.sqsClient.SendMessageAsync(request);
+                
+                    // 檢查 response
+                    if (sendMessageResponse.HttpStatusCode != HttpStatusCode.OK)
+                    {
+                        await this.errorNotifyService.SendErrorMessage($"ErrorRetryService 呼叫 sqsClient 失敗。HTTP 狀態碼: {sendMessageResponse.HttpStatusCode}");
                     }
+                
+                    this.logger.LogInformation($"快照重試第{retryCount + 1}次，傳送訊息至 SQS 成功");
+                
+                    // 刪除舊訊息
+                    var receiptHandle = message.ReceiptHandle;
+                    await this.queueDeleteService.DeleteMessage(receiptHandle);
+                
+                    this.logger.LogInformation($"快照重試第{retryCount + 1}次，刪除舊訊息成功");
                 }
             }
-            catch (Newtonsoft.Json.JsonException e)
+            catch (JsonException e)
             {
-                await this.errorNotifyService.sendErrorMessage($"ErrorRetryService queueMessage JsonException: {e.Message}", context);
-            }
-        }
-
-        /// <summary>
-        /// 刪除舊訊息
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="context"></param>
-        private async Task deleteErrorMessage(SQSEvent.SQSMessage message, ILambdaContext context)
-        {
-            try
-            {
-                var request = new DeleteMessageRequest
-                {
-                    QueueUrl = "your queue url",  // ex. https://sqs.ap-northeast-1.amazonaws.com/123456789/ScreenShot_Queue
-                    ReceiptHandle = message.ReceiptHandle,
-                };
-
-                var deleteMessageResponse = await this.sqsClient.DeleteMessageAsync(request);
+                await this.errorNotifyService.SendErrorMessage($"ErrorRetryService queueMessage JsonException: {e.Message}");
             }
             catch (AmazonSQSException e)
             {
-                await this.errorNotifyService.sendErrorMessage($"刪除 SQS 訊息發生錯誤：{e.Message}", context);
+                await this.errorNotifyService.SendErrorMessage($"快照重試失敗， SQS 異常：{e.Message}");
+            }
+            catch (Exception e)
+            {
+                await this.errorNotifyService.SendErrorMessage($"快照重試時發生錯誤：{e.Message}");
             }
         }
     }
