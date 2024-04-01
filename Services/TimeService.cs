8de48f235638a16a83c40dcd5c9389d4271362d7
Andy Wei <48671893+Andy0909@users.noreply.github.com>
2024-04-01 15:39:59 +0800

    Update TimeService.cs

--- Services/TimeService.cs
+++ Services/TimeService.cs
@@ -1,9 +1,18 @@
 ﻿namespace ScreenShotLambda.Services
 {
+    /// <summary>
+    /// 取得時間服務
+    /// </summary>
     public class TimeService
     {
+        /// <summary>
+        /// 台北時區的 TimeZoneInfo 物件
+        /// </summary>
         private readonly TimeZoneInfo taipeiTimeZone;
 
+        /// <summary>
+        /// 建構子
+        /// </summary>
         public TimeService()
         {
             // 使用台灣標準時間的時區
@@ -11,7 +20,10 @@
 
         }
 
-        public DateTime getCurrentTaipeiTime()
+        /// <summary>
+        /// 取得當前台北時間
+        /// </summary>
+        public DateTime GetCurrentTaipeiTime()
         {
             return TimeZoneInfo.ConvertTime(DateTime.Now, taipeiTimeZone);
         }
